397ed4612cba9380de750c3969bcc0f480a028cf
Andrew <ahernandez@codeforamerica.org>
2023-04-28 11:29:17 -0700

    Add project files.

---
+++ C# Data Dashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C__Data_Dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
